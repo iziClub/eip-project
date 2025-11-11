da81edbc2858866a62c69289f3fe05c1b53737d2
teofeo <teo.franoux@epitech.eu>
2025-11-10 19:24:55 -0500

    feat(save) : First try

---
+++ monorepo/apps/api/app/models/user.ts
@@ -0,0 +1,46 @@
+import { DateTime } from 'luxon'
+import hash from '@adonisjs/core/services/hash'
+import { compose } from '@adonisjs/core/helpers'
+import { BaseModel, column, beforeSave } from '@adonisjs/lucid/orm'
+import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import { DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
+
+const AuthFinder = withAuthFinder(() => hash.use(), {
+  uids: ['email'],
+  passwordColumnName: 'password',
+})
+
+export default class User extends compose(BaseModel, AuthFinder) {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column({ columnName: 'first_name' })
+  declare firstName: string
+
+  @column({ columnName: 'last_name' })
+  declare lastName: string
+
+  @column()
+  declare email: string
+
+  @column({ serializeAs: null })
+  declare password: string
+
+  @column()
+  declare role: 'USER' | 'ADMIN'
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime | null
+
+  static accessTokens = DbAccessTokensProvider.forModel(User)
+
+  @beforeSave()
+  static normalizeEmail(user: User) {
+    if (user.email) {
+      user.email = user.email.toLowerCase()
+    }
+  }
+}
